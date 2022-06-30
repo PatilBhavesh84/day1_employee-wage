2a7e634d90733f0aeac17103fcc95cb265f98994
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-06-30 13:34:52 +0530

    Added:Welcome message

---
+++ src/com/bridgelabz/emp_wage/Emp_wage_computation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.emp_wage;
+
+public class Emp_wage_computation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+
+	}
+
+}
